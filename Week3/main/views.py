fe627353130e95defb9e57c6ac8ff73b66dd6297
Akbota <bmautkazy@gmail.com>
2018-10-18 02:14:39 +0600

    was pulled, deleted

---
+++ Week3/main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from datetime import datetime, timedelta
+
+def todo(request):
+    tasks = [{
+        'name': 'Task {}'.format(i),
+        'created': datetime.today().strftime("%d/%m/%y"),
+        'due_to': (datetime.today() + timedelta(days=2)).strftime("%d/%m/%y"),
+        'owner': 'admin',
+        'mark': True
+        }
+        for i in range(1, 5)
+    ]
+    context = {
+        'list': tasks
+    }
+    return render(request, 'todo_list.html', context)
+
+def completedTodo(request, number):
+    tasks = [{
+        'name': 'Task {}'.format(number),
+        'created': datetime.today().strftime("%d/%m/%y"),
+        'due_to': (datetime.today() + timedelta(days=2)).strftime("%d/%m/%y"),
+        'owner': 'admin',
+        'mark': True
+        }
+    ]
+    context = {
+        'list': tasks
+    }
+    return render(request, 'completed_todo_list.html', context)
